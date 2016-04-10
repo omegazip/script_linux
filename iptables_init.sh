92fc098f1a2ef14245e73ef37fb583cbc001181d
Omega Zip <abner2k5@gmail.com>
2016-04-10 12:29:02 -0600

    First commit

---
+++ iptables_init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# IPv4
+
+iptables -F
+iptables -X
+iptables -Z
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
